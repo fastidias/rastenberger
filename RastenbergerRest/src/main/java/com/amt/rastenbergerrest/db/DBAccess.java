6adbe77329e60827ef42c2f702d2492fde869c00
Alexander Weiß <a.weiss@epages.com>
2017-08-07 13:59:37 +0200

    added getters

--- RastenbergerRest/src/main/java/com/amt/rastenbergerrest/db/DBAccess.java
+++ RastenbergerRest/src/main/java/com/amt/rastenbergerrest/db/DBAccess.java
@@ -2,46 +2,49 @@ package com.amt.rastenbergerrest.db;
 
 import com.amt.rastenbergerrest.models.FoodOffer;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class DBAccess {
-   private static final Map<Long,FoodOffer> FOOD_OFFERS = new HashMap<>();
+   private static final List<FoodOffer> FOOD_OFFERS = new ArrayList<>();
    private static long index;
    
    static {
-       FOOD_OFFERS.put(
-               ++index,
-               new FoodOffer("t.gießler","http://test1.de","ich bins der tobi",index)
+       FOOD_OFFERS.add(
+               new FoodOffer("t.gießler","http://test1.de","ich bins der tobi",getNextID())
        );
        
-       FOOD_OFFERS.put(
-               ++index,
-               new FoodOffer("m.jäger","http://test2.de","ich bins der masl",index)
+       FOOD_OFFERS.add(
+               new FoodOffer("m.jäger","http://test2.de","ich bins der masl",getNextID())
        );
        
-       FOOD_OFFERS.put(
-               ++index,
-               new FoodOffer("a.weiß","http://test3.de","ich bins der alex",index)
+       FOOD_OFFERS.add(
+               new FoodOffer("a.weiß","http://test3.de","ich bins der alex",getNextID())
        );        
    }
    
    public List<FoodOffer> getFoodOffers(){
-       return new ArrayList<>(FOOD_OFFERS.values());
+       return FOOD_OFFERS;
        
    }
    
-   public FoodOffer addFoodOffer(FoodOffer foodOffer) {
-       FOOD_OFFERS.put(
-               ++index,
+   public FoodOffer addFoodOffer(FoodOffer foodOffer) {       
+       foodOffer.setId(getNextID());
+
+       FOOD_OFFERS.add(
                foodOffer
        );        
        
-       foodOffer.setId(index);
-       
        return foodOffer;
    }
+   
+    private static Long getNextID() {
+        
+        return ++index;
+    }
+    
+    private static Long getID() {
+        return index;
+    }
    
    public FoodOffer updateFoodOffer(Long index, FoodOffer foodOffer) {
        
