d50c81e84899536472bfc8345f75835cdc5523f8
Tobias Gießler <t.giessler@epages.com>
2017-09-03 13:38:42 +0200

    Inject EntityMangerFactory.

--- RastenbergerRest/src/main/java/com/amt/rastenbergerrest/service/FoodOfferService.java
+++ RastenbergerRest/src/main/java/com/amt/rastenbergerrest/service/FoodOfferService.java
@@ -4,18 +4,17 @@ import com.amt.rastenbergerrest.db.FoodOfferEntity;
 import com.amt.rastenbergerrest.models.FoodOffer;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.persistence.Query;
 
 public class FoodOfferService {
 
-    private static final String PERSISTENCE_UNIT_NAME = "com.amt_RastenbergerRest_war_0.1PU";
+    @Inject
     private EntityManagerFactory factory;
 
     public List<FoodOffer> getFoodOffers() {
-        factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
         EntityManager em = factory.createEntityManager();
 
         Query query = em.createQuery("SELECT e FROM FoodOfferEntity e");
@@ -37,7 +36,6 @@ public class FoodOfferService {
     }
 
     public FoodOffer updateFoodOffer(final FoodOffer foodOffer) {
-        factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
         EntityManager em = factory.createEntityManager();
 
         final FoodOfferEntity entity = em.find(
@@ -57,7 +55,6 @@ public class FoodOfferService {
     }
 
     public FoodOffer deleteFoodOffer(final FoodOffer foodOffer) {
-        factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
         EntityManager em = factory.createEntityManager();
 
         final FoodOfferEntity entity = em.find(
